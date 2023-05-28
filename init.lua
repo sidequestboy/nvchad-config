5ad2ea72cef7c269960d2100b7e19291964ce090
Jamie Macdonald <jamie.alban@gmail.com>
2023-05-28 00:04:35 -0700

    Add some neovide settings

--- init.lua
+++ init.lua
@@ -5,6 +5,16 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+if vim.g.neovide then
+  vim.o.guifont = "FiraCode Nerd Font Mono:h14"
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.keymap.set("i", "<D-v>", "<C-r>*")
+  vim.keymap.set("v", "<D-c>", "\"*y")
+end
+
 vim.o.relativenumber = true;
 vim.o.colorcolumn = "80"
 vim.o.nrformats = vim.o.nrformats .. ",unsigned"
